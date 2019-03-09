3797702aa3005e3479f9af44ba99c94ed413f736
Yaye Fatou Thiam <yayefatouthiam@dhcp-10-105-223-251.wireless.northwestern.private>
2019-03-09 13:02:04 -0600

    Updated navbar

--- client/src/App.js
+++ client/src/App.js
@@ -27,22 +27,13 @@ class App extends Component {
     // Filter this.state.friends for friends with an id equal to the id of the card being clicked
     const friends = this.state.friends;
     const cardClicked = friends.filter(friend => friend.id === id);
- 
+    console.log(friends);
     // If a card is already clicked => clicked = true
-    if (cardClicked[0].clicked) {
-      // the score is reinitialized
-      score = 0;
-      message = " You guessed incorrectly - Click again to start over! ";
-      //the clicked value of all the cards is reinitilaized to false
-      for (let i = 0; i < friends.length; i++) {
-				friends[i].clicked = false;
-      }
-
-      this.setState({ friends, score, message });
-     
-    }
-    else {// clicked = false ==> card not clicked
-      cardClicked[0].clicked = true;
+    if 
+    // (cardClicked[0].clicked)
+      (! cardClicked[0].clicked) {
+        console.log("if: " + cardClicked[0].clicked);
+        cardClicked[0].clicked = true;
       score++;
       message = " You guessed correctly ";
 
@@ -61,6 +52,39 @@ class App extends Component {
       
       this.setState({ friends, score, message });
     }
+    else {
+      // the score is reinitialized
+      console.log("else: " + cardClicked[0].clicked);
+      score = 0;
+      message = " You guessed incorrectly - Click again to start over! ";
+      //the clicked value of all the cards is reinitilaized to false
+      for (let i = 0; i < friends.length; i++) {
+				friends[i].clicked = false;
+      }
+
+      this.setState({ friends, score, message });
+     
+    }
+    // else {// clicked = false ==> card not clicked
+    //   cardClicked[0].clicked = true;
+    //   score++;
+    //   message = " You guessed correctly ";
+
+    //   // If the player score is higher than the previous topscore
+    //   if (score > topscore) {
+    //     topscore = score; // The new topscore is the score
+    //     this.setState({score, topscore })
+    //   }
+
+    //   if (topscore === 12) {
+    //     message = " YAY, You won!! Click a card to play again! ";
+    //   }
+
+    //   // Shuffle the cards
+    //   friends.sort(() => Math.random() - 0.5);
+      
+    //   this.setState({ friends, score, message });
+    // }
   };
 
   
